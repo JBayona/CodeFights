d14396718d87609f638f018908c076c835ca9820
JBayona <jordavids_22@hotmail.com>
2017-03-30 23:49:19 -0600

    buildPalindrome explanation

--- Intro Mode/buildPalindrome.js
+++ Intro Mode/buildPalindrome.js
@@ -13,11 +13,19 @@ function buildPalindrome(st) {
   var array = [...''+st];
   var i = 0;
   var j = array.length-1;
+  /*En el momento en que se crucen las variables ya terminamos
+  de analizar y podemos regresar el resultado*/
   while(i < j){
+  	/*Si no son iguales insertamos en la posción j+1 el elemento
+  	de la izquierda que no es igual, debemos actualizar j sumándole
+  	uno para que a la siguiente vuelta nos verifique que ahora son
+  	iguales*/
     if(array[i] !== array[j]){
       array.splice(j+1,0, array[i]);
       j += 1;
     }else{
+   	/*Si son iguales solo debemos recorrer las variables que recorren
+   	de izquierda a derecha y derecha a izquierda*/
       i++;
       j--;
     }
