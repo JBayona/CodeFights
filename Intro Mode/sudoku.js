df5bc569d5976c2822e6de1bf8f2b076f528864f
JBayona <jordavids_22@hotmail.com>
2017-04-08 18:13:48 -0500

    sudoku intro mode level 12

--- Intro Mode/sudoku.js
+++ Intro Mode/sudoku.js
@@ -12,47 +12,86 @@ each of the nine 3 × 3 sub-grids should contain all of the digits from 1 to 9.
 https://codefights.com/arcade/intro/level-12/tQgasP8b62JBeirMS/description
 */
 
-function sudoku(grid) {
-  var checkBlock = function(block) {
-    var sample = '123456789';
-    block.sort();
-    if (block.join('') === sample) {
-      return true;
-    }
-    return false;
-  }
+//Basic idea: This is a stupid problem that anyone can do.
+//  If I were given this in an interview, I'd reconsider the company.
 
-  var subgrids = [];
+//Turn columns into rows
+var transpose = grid =>
+    grid[0].map(
+        (_,c) => grid.map(
+            row => row[c]
+        )
+    )
 
-  for (var i = 0; i < 3; i++) {
-    subgrids.push([]);
-    for (var j = 0; j < 3; j++) {
-      subgrids[i].push([]);
-    }
-  }
-
-  for (var i = 0; i < 9; i++) {
-    var horizontal = [];
-    var vertical = [];
-    for (var j = 0; j < 9; j++) {
-      horizontal.push(grid[i][j]);
-      vertical.push(grid[j][i]);
-      subgrids[Math.floor(i / 3)][Math.floor(j / 3)].push(grid[i][j]);
-    }
-    if (!checkBlock(horizontal)) {
-      return false;
-    }
-    if (!checkBlock(vertical)) {
-      return false;
-    }
-  }
-
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-      if (!checkBlock(subgrids[i][j]))
-        return false;
-      }
-    }
-
-  return true;
+//Turn sub-grids into rows
+var expandSubGrids = grid => {
+    var three = Math.sqrt(grid.length);
+    return grid[0].map(
+        (_,i) => grid[0].map(
+            (_,j) => grid[(i/three|0)*three+j/three|0][i%three*three+j%three]
+        )
+    )
 }
+
+//Check a given row for duplicates
+var checkRow = row => {
+    var count = [];
+    for(var cell of row) {
+        if(cell != '.') {
+            if(count[cell])
+                return false;
+            count[cell] = 1;
+        }
+    }
+    return true;
+}
+
+var sudoku = grid =>
+    grid.every(checkRow) &&
+    transpose(grid).every(checkRow) &&
+    expandSubGrids(grid).every(checkRow)
+
+
+//Opcion 2
+
+function sudoku2(grid) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            var c = grid[i][j];
+            if (c !== '.') {
+                // check row
+                for (var z = 0; z < 9; z++) {
+                    if (j !== z && grid[i][z] === c)
+                        return false;
+                }
+                // check column
+                for (var z = 0; z < 9; z++) {
+                    if (i !== z && grid[z][j] === c)
+                        return false;
+                }
+                // check square
+                var a = i - i%3,
+                    b = j - j%3;
+                for (var x = a; x < a+3; x++) {
+                    for (var y = b; y < b+3; y++) {
+                        if (x !== i && y !== j && grid[x][y] === c)
+                            return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+
+//Opcion 3
+
+// True, if product of #s in row is 9!
+p = a => eval(a.join`*`)==362880
+
+// Check each row, column and 3 block grid using p() 
+sudoku = grid => 
+    grid.every((r,i) =>
+               p(r) &&
+               p(grid.map(r => r[i])) &&
+               p(r.map((_,j) => grid[3*(i/3|0)+(j/3|0)][3*(i%3)+(j%3)])))
