ca15f5bd684fd5c78c5b6d9d7b8560bc22de27cf
JBayona <jordavids_22@hotmail.com>
2017-04-09 15:52:24 -0500

    concatenateArrays

---
+++ Arcade Core/List Forest Edge/concatenateArrays.js
@@ -0,0 +1,12 @@
+/*
+Given two arrays of integers a and b, obtain the array formed by the elements of a followed by the elements of b.
+
+Example
+
+For a = [2, 2, 1] and b = [10, 11], the output should be
+concatenateArrays(a, b) = [2, 2, 1, 10, 11].
+
+https://codefights.com/arcade/code-arcade/list-forest-edge/GeqSK26bvdrarkGH9
+*/
+
+concatenateArrays = (a, b) => a.concat(b) 
