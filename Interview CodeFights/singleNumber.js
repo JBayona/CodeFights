91fd1bd16faf511274bd16d9ea168033724a2be0
JBayona <jordavids_22@hotmail.com>
2017-04-13 18:38:57 -0500

    singleNumber Interview Section

---
+++ Interview CodeFights/singleNumber.js
@@ -0,0 +1,28 @@
+/*
+You are given an array of integers in which every element appears twice, except for one. Find the element that only appears one time. Your solution should have a linear runtime complexity (O(n)). Try to implement it without using extra memory.
+
+Example
+
+For nums = [2, 2, 1], the output should be
+singleNumber(nums) = 1.
+
+https://codefights.com/interview/7xqHvuKXT2oBJ26iq/description
+*/
+
+//My version
+
+function singleNumber(nums) {
+    var result = 0;
+    nums.forEach(num => result ^= num);
+    return result;
+}
+
+//Grits version
+function singleNumber(nums) {
+    for(i in nums){
+        if(nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) return nums[i]
+    }
+}
+
+nums = [2, 2, 1];
+console.log(singleNumber(nums));
