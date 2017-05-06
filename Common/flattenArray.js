b48487405d49173bcbfd425be8237f0648de5d9f
JBayona <jordavids_22@hotmail.com>
2017-05-05 22:32:24 -0500

    flattenArray common

---
+++ Common/flattenArray.js
@@ -0,0 +1,12 @@
+function flatten(array){
+    return array.reduce(function(acum, val){
+      return acum.concat(Array.isArray(val) ? flatten(val) : val)
+    }, []);
+  }
+
+//Equivalente a
+
+const flatten = array => array.reduce((acum,val) => acum.concat(Array.isArray(val) ? flatten(val) : val), []);
+
+array = [1,2,3,[4,5,6,[7,8,9,[10,11,12,[13,14,15,[16]]]]]];
+console.log(flatten(array));
