1f71f9a0d9ef10080a1be22bc5f920389d76f45c
JBayona <jordavids_22@hotmail.com>
2017-05-23 22:03:22 -0500

    Base Util convertion problem description

--- Common/totalBinSum.js
+++ Common/totalBinSum.js
@@ -1,7 +1,6 @@
 /*
-/*
-Given a number num in its binary representation, your task is to sum all the numbers in base 10 formed by the prefixes of num. 
-More formally, sum up int(num[0, i]) for all possible i.
+Given a number num in its binary representation, your task is to sum all the numbers in base 10 formed by the 
+prefixes of num. More formally, sum up int(num[0, i]) for all possible i.
 Since the answer can be very big, return it modulo 109 + 7.
 
 Example
