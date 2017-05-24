2070ccd139e87ca35fbba5c45733a2160c6232c0
JBayona <jordavids_22@hotmail.com>
2017-05-23 22:00:32 -0500

    Base description

--- Common/totalBinSum.js
+++ Common/totalBinSum.js
@@ -11,10 +11,10 @@ totalBinSum(bin) = 16.
 
 Here are all the prefixes:
 
-1 = 110;
-10 = 210;
-100 = 410;
-1001 = 910.
+1 = 1;
+10 = 2;
+100 = 4;
+1001 = 9.
 Thus, the answer is 1 + 2 + 4 + 9 = 16.
 
 https://codefights.com/challenge/prd5XH6KJtWPRva4w
