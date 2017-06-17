f325d519218db83ce9b93b0e5f139840445c3a1d
JBayona <jordavids_22@hotmail.com>
2017-06-17 18:39:37 -0500

    numberOf1Bits url

--- Interview CodeFights/numberOf1Bits.js
+++ Interview CodeFights/numberOf1Bits.js
@@ -4,6 +4,8 @@ Write the solution that uses O(k) operations per test, where k is the number of 
 
 Write a function that takes an unsigned (positive) integer and returns the number of 1 bits 
 its binary representation contains. This value is also known as the integer's Hamming weight.
+
+https://codefights.com/interview/fDtqXxcFykCQfrqay
 */
 
 //Option 1
