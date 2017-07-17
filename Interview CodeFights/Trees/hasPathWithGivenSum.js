eb24dea4d16a3b9feb6c6e53f1cae4883b288a73
JBayona <jordavids_22@hotmail.com>
2017-07-17 00:02:27 -0500

    hasPathWithGivenSum tree

---
+++ Interview CodeFights/Trees/hasPathWithGivenSum.js
@@ -0,0 +1,171 @@
+/*
+Given a binary tree t and an integer s, determine whether there is a root to leaf path in t such that the sum of vertex values equals s.
+
+Example
+
+For
+
+t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -2,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+and
+s = 7,
+the output should be hasPathWithGivenSum(t, s) = true.
+
+This is what this tree looks like:
+
+      4
+     / \
+    1   3
+   /   / \
+  -2  1   2
+    \    / \
+     3  -2 -3
+Path 4 -> 3 -> 2 -> -2 gives us 7, the required sum.
+
+For
+
+t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -4,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+and
+s = 7,
+the output should be hasPathWithGivenSum(t, s) = false.
+
+This is what this tree looks like:
+
+      4
+     / \
+    1   3
+   /   / \
+  -2  1   2
+    \    / \
+     3  -4 -3
+There is no path from root to leaf with the given sum 7.
+
+https://codefights.com/interview-practice/task/TG4tEMPnAc3PnzRCs
+*/
+
+//t is the tree and s is the number of the sum
+/*Va a ir analizando recursivamente cada nodo hasta encontrar null, se van a ir
+retornando los resultados hasta encontrar si hay una ruta en donde se cumpla
+si se puede dar con dicho valor, al hacerlo recursivo, cuando regresa va
+analizando izquierda y derecha en cada nodo*/
+function hasPathWithGivenSum(t, s) {
+    
+    if (t === null) return s === 0;
+    var remain = s - t.value;
+    return hasPathWithGivenSum(t.left, remain) || hasPathWithGivenSum(t.right, remain);
+}
+
+
+var tree = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -2,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+var s = 7;
+console.log(hasPathWithGivenSum(tree, s));
+
+
+
