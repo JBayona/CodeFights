7802025e3d2bcecc6ecd8b2d131b6caad87d4dcb
JBayona <jordavids_22@hotmail.com>
2017-09-24 13:22:32 -0500

    Permutation Heap Algorithm

---
+++ Common/permutationsHeap.js
@@ -0,0 +1,44 @@
+/*
+Este algoritmo usa el heap algorithm para hacer todas las 
+permutaciones posibles
+*/
+
+var permute = function(str){
+  var result = [];
+  heapPermutation([...str], str.length, str.length, result);
+  return result;
+}
+
+//Generating permutation using Heap Algorithm
+function heapPermutation(a, size, n, result){
+    // if size becomes 1 then prints the obtained
+    // permutation
+    if (size == 1){
+      result.push(a.join(''));
+      //result.push(a);
+      //console.log(a);
+    }
+
+    for (var i=0; i<size; i++){
+        heapPermutation(a, size-1, n, result);
+
+        // if size is odd, swap first and last
+        // element
+        if (size % 2 == 1){
+            var temp = a[0];
+            a[0] = a[size-1];
+            a[size-1] = temp;
+        }
+
+        // If size is even, swap ith and last
+        // element
+        else{
+            var temp = a[i];
+            a[i] = a[size-1];
+            a[size-1] = temp;
+        }
+    }
+}
+
+str = "abcde";
+console.log(permute(str));
