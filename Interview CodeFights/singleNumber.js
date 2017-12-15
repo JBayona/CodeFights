7b1e0fb2f4af3c7bd888b45b175a643dec6aa7ce
JBayona <jordavids_22@hotmail.com>
2017-12-15 11:02:37 -0600

    Single Number

--- Interview CodeFights/singleNumber.js
+++ Interview CodeFights/singleNumber.js
@@ -1,5 +1,6 @@
 /*
-You are given an array of integers in which every element appears twice, except for one. Find the element that only appears one time. Your solution should have a linear runtime complexity (O(n)). Try to implement it without using extra memory.
+You are given an array of integers in which every element appears twice, except for one. Find the element that only appears one time. 
+Your solution should have a linear runtime complexity (O(n)). Try to implement it without using extra memory.
 
 Example
 
