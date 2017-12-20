4a598f3cfaef1d2c13b040ae80d53613bcac35b5
JBayona <jordavids_22@hotmail.com>
2017-12-19 18:02:16 -0600

    Missing Number

--- Interview CodeFights/missingNumber.js
+++ Interview CodeFights/missingNumber.js
@@ -1,4 +1,5 @@
 /*
+
 You are supposed to label a bunch of boxes with numbers from 0 to n, and all the labels are stored in the array arr. Unfortunately one of the labels is missing. Your task is to find it.
 
 Example
@@ -7,6 +8,7 @@ For arr = [3, 1, 0], the output should be
 missingNumber(arr) = 2.
 
 https://codefights.com/interview/t8iNoBtDf5XSiwHiF/companies/PBvcD79bDSCacJQff/description
+
 */
 
 function missingNumber(arr) {
